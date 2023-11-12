f6a1bf280d94b3aebab2b3aaa8a32574ef45be9b
olimat12 <matt.olivero@hotmail.com>
2023-11-12 10:57:24 -0600

    Changes to trivia class and file

--- app/src/main/java/edu/utsa/rqo215_lab4/model/Trivia.java
+++ app/src/main/java/edu/utsa/rqo215_lab4/model/Trivia.java
@@ -2,6 +2,7 @@ package edu.utsa.rqo215_lab4.model;
 
 import android.app.Activity;
 import android.content.res.AssetManager;
+import android.util.Log;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -33,7 +34,54 @@ public class Trivia {
         System.out.println("Explicit Trivia default constructor");
     }
 
-    //TODO: Write the setters and getters for all the instance variables
+    //Setters and getters
+    public void setQuestion(String question){
+        this.question = question;
+    }
+
+    public String getQuestion(){
+        return this.question;
+    }
+
+    public void setOption1(String option1){
+        this.option1 = option1;
+    }
+
+    public String getOption1(){
+        return this.option1;
+    }
+
+    public void setOption2(String option2){
+        this.option2 = option2;
+    }
+
+    public String getOption2(){
+        return this.option2;
+    }
+
+    public void setOption3(String option3){
+        this.option3 = option3;
+    }
+
+    public String getOption3(){
+        return this.option3;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setCorrectAnswer(String correctAnswer){
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getCorrectAnswer(){
+        return this.correctAnswer;
+    }
 
     /*
     @param: Activity activity
@@ -69,6 +117,7 @@ public class Trivia {
             String line = "";
             while(j < lineNumber){
                 line = scanner.nextLine();
+                j++;
             }
             //When loop is over, we are processing the right directly before the one we want to return, so...
             line = scanner.nextLine();
@@ -79,18 +128,17 @@ public class Trivia {
             this.option3 = lineSplit[3].trim();
             this.description = lineSplit[4];
             //From index 5 forward is the description
-            for(int k = 5; k < lineSplit.length; k++){
+            for(int k = 5; k < lineSplit.length; k++) {
                 this.description = this.description + "," + lineSplit[k];
             }
-
+            identifyCorrectAnswer();
             return (this);
-
         }
         catch(FileNotFoundException e){
-            System.err.println("File not found");
+            Log.d("File Exception", "File not found");
         }
         catch(IOException e){
-            System.err.println("IO exception, which is super class of FileNotFoundException");
+            Log.d("File Exception","IO exception, which is super class of FileNotFoundException");
         }
 
         return (this);
@@ -98,6 +146,14 @@ public class Trivia {
 
     private void identifyCorrectAnswer(){
         //takes all the options and check if the description contains any of the options, if so, that option becomes the correct answer
-
+        if(this.description.contains(this.option1)){
+            this.correctAnswer = this.option1;
+        }
+        else if(this.description.contains(this.option2)){
+            this.correctAnswer = this.option2;
+        }
+        else{
+            this.correctAnswer = this.option3;
+        }
     }
 }
